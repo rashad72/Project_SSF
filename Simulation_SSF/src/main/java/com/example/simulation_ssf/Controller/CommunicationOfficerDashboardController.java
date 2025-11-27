b20d04ffc83dfc8adcf465f88c82b4b77cb913f9
toufikur-r38 <toufikur.private@gmail.com>
2025-11-27 12:48:09 +0600

    controller & dashboard Update

---
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/CommunicationOfficerDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.simulation_ssf.Controller;
+
+import javafx.event.ActionEvent;
+
+public class CommunicationOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void communicationRecordsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void communicationPanelButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyLogsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorFieldOperationsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generateReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void incomingMessagesButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void officerAvailabilityButtonOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/OperationCommanderDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.simulation_ssf.Controller;
+
+import javafx.event.ActionEvent;
+
+public class OperationCommanderDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void reopenCompletedMissionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignBackupTeamButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createNewMissionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void missionStatusUpdateButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reassignTeamCaptainButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewOngoingMissionsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void missionProposalsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cancelOrPostponeMissionButtonOnAction(ActionEvent actionEvent) {
+    }
+}
