10cbe44d6678f226526de85912afd38ec9d6971b
Ritom766 <tahsinnafis25@gmail.com>
2025-12-03 22:14:58 +0600

    2 User Dashbord Fxml

---
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/EquipmentManagerDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.simulation_ssf.Controller;
+
+import javafx.event.ActionEvent;
+
+public class EquipmentManagerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void RemoveEquipmentButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void EquipmentHistoryButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AvailableEquipmentButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateEquipmentButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void GenerateMaintenanceReportButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AlertCenterButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void MaintenanceScheduleButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AddEquipmentButtonOnAction(ActionEvent actionEvent) {
+    }
+}
