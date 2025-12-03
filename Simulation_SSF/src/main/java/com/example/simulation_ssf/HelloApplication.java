cdeafdd9db6d414f4af75eaba155aa893eeddfb9
toufikur-r38 <toufikur.private@gmail.com>
2025-12-03 22:45:08 +0600

    u1

--- Simulation_SSF/src/main/java/com/example/simulation_ssf/HelloApplication.java
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/HelloApplication.java
@@ -10,13 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<< HEAD
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/simulation_ssf/DashboardOfUsers/CommunicationOfficerDashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-=======
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logInView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
->>>>>>> 653b48620cf3410cfc42ec70df567b32350d191a
+       // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logInView.fxml"));
+     //   Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
