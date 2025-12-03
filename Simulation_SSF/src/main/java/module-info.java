4f44ec423dc6be01ce65fe741e075a4eef52061c
toufikur-r38 <toufikur.private@gmail.com>
2025-12-03 22:09:01 +0600

    Fixing bug

--- Simulation_SSF/src/main/java/module-info.java
+++ Simulation_SSF/src/main/java/module-info.java
@@ -1,9 +1,11 @@
 module com.example.simulation_ssf {
     requires javafx.controls;
     requires javafx.fxml;
-    requires com.example.simulation_ssf;
+   // requires com.example.simulation_ssf;
 
 
     opens com.example.simulation_ssf to javafx.fxml;
+    // new add
+    opens com.example.simulation_ssf.Controller to javafx.fxml;
     exports com.example.simulation_ssf;
 }
