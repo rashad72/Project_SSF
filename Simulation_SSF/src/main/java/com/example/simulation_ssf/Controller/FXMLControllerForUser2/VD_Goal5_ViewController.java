6c1eb72a3bb9fb730587945db5c0dd1b2d166b28
rashad72 <2420206@iub.edu.bd>
2025-12-06 03:12:25 +0600

    Update2

--- Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/FXMLControllerForUser2/VD_Goal5_ViewController.java
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/FXMLControllerForUser2/VD_Goal5_ViewController.java
@@ -1,12 +1,20 @@
 package com.example.simulation_ssf.Controller.FXMLControllerForUser2;
 
 import com.example.simulation_ssf.SSFApplication;
+import com.example.simulation_ssf.nonUser.AppendableObjectOutputStream;
+import com.example.simulation_ssf.nonUser.Message;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class VD_Goal5_ViewController
 {
@@ -23,10 +31,15 @@ public class VD_Goal5_ViewController
     @javafx.fxml.FXML
     private TextField cmcMessageSubjectTextField;
     @javafx.fxml.FXML
-    private ComboBox cmcMissionTypeComboBox;
+    private ComboBox<String> cmcMissionTypeComboBox;
+
+    private ArrayList<Message> messageList = new ArrayList<>();
+    @javafx.fxml.FXML
+    private TextField cmcMessageIdTextField;
 
     @javafx.fxml.FXML
     public void initialize() {
+        cmcMissionTypeComboBox.getItems().addAll("Rescue","Surveillance","Protection");
     }
 
     @javafx.fxml.FXML
@@ -49,5 +62,33 @@ public class VD_Goal5_ViewController
 
     @javafx.fxml.FXML
     public void cmcSendMessageButtonOnAction(ActionEvent actionEvent) {
+        //int messageId, int senderId, int receiverId, int missionId, String subject, String content, LocalDate timestamp, String status, String missionType
+        Message m = new Message(Integer.parseInt(cmcMessageIdTextField.getText()),Integer.parseInt(cmcEmployeeIdTextField.getText()),
+                00,Integer.parseInt(cmcMissionIdTextField.getText()),cmcMessageSubjectTextField.getText(),
+                cmcMessageBodyTextArea.getText(),cmcDatePicker.getValue(),"UNREAD",cmcMissionTypeComboBox.getValue());
+        messageList.add(m);
+
+        try {
+            File messageFile = new File("Message.bin");
+            FileOutputStream fos = null;
+            ObjectOutputStream oos = null;
+
+            if (messageFile.exists()){
+                fos = new FileOutputStream(messageFile, true);
+
+                oos = new AppendableObjectOutputStream(fos);
+                System.out.println("appendable");
+            }
+            else {
+                fos = new FileOutputStream(messageFile);
+                System.out.println("new");
+                oos = new ObjectOutputStream(fos);
+            }
+            oos.writeObject(m);
+            oos.close();
+            System.out.println("Object saved");
+        } catch (Exception e) {
+            System.out.println("Not saved");;
+        }
     }
 }
