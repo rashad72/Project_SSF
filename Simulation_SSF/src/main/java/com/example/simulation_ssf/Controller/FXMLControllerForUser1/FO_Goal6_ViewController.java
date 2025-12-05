192e3cfe88aaaaa09fb517c0fbc5ce47077ec96f
toufikur-r38 <toufikur.private@gmail.com>
2025-12-05 18:13:36 +0600

    bug fixing

--- Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/FXMLControllerForUser1/FO_Goal6_ViewController.java
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/FXMLControllerForUser1/FO_Goal6_ViewController.java
@@ -2,6 +2,7 @@ package com.example.simulation_ssf.Controller.FXMLControllerForUser1;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
--- Simulation_SSF/src/main/java/com/example/simulation_ssf/nonUser/Mission.java
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/nonUser/Mission.java
@@ -97,14 +97,9 @@ public class Mission implements Serializable {
     public void setHasBackup(boolean hasBackup) {
         this.hasBackup = hasBackup;
     }
-
-<<<<<<< HEAD
-    public void setBackupTeam(Team backupTeam) {this.backupTeam = backupTeam;}
-=======
     public void setCompletionDate(LocalDate completionDate) {
         this.completionDate = completionDate;
     }
->>>>>>> d23a171362ca59cc7abe756fe5005c9a512b2d95
 
     @Override
     public String toString() {
