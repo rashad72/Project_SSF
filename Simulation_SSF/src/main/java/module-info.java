b0dc884dcc2b35ba7b2c14035b7c5f32d75d4df3
toufikur-r38 <toufikur.private@gmail.com>
2025-12-06 00:13:29 +0600

    1

--- Simulation_SSF/src/main/java/module-info.java
+++ Simulation_SSF/src/main/java/module-info.java
@@ -8,8 +8,11 @@ module com.example.simulation_ssf {
     // new add
     opens com.example.simulation_ssf.Controller to javafx.fxml;
     //new
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser1 to javafx.fxml;
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser2 to javafx.fxml;
     opens com.example.simulation_ssf.Controller.FXMLControllerForUser3 to javafx.fxml;
     opens com.example.simulation_ssf.Controller.FXMLControllerForUser4 to javafx.fxml;
     opens com.example.simulation_ssf.Controller.FXMLControllerForUser5 to javafx.fxml;
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser6 to javafx.fxml;
     exports com.example.simulation_ssf;
 }
