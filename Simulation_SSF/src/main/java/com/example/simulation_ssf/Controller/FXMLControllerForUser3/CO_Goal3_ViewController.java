b27d0fd48d44702ab7ce7a87e651bdbad5e3b75b
toufikur-r38 <toufikur.private@gmail.com>
2025-12-06 06:18:49 +0600

    u1

--- Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/FXMLControllerForUser3/CO_Goal3_ViewController.java
+++ Simulation_SSF/src/main/java/com/example/simulation_ssf/Controller/FXMLControllerForUser3/CO_Goal3_ViewController.java
@@ -1,6 +1,7 @@
 package com.example.simulation_ssf.Controller.FXMLControllerForUser3;
 
 import com.example.simulation_ssf.SSFApplication;
+import com.example.simulation_ssf.nonUser.Message;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,18 +10,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class CO_Goal3_ViewController
 {
     @javafx.fxml.FXML
-    private TableColumn teamCaptainIdTableColumn;
-    @javafx.fxml.FXML
     private TableColumn messageIdTableColumn;
     @javafx.fxml.FXML
     private TableColumn missionIdTableColumn;
-    @javafx.fxml.FXML
-    private TableColumn senderTableColumn;
     @javafx.fxml.FXML
     private TableColumn missionTypeTableColumn;
     @javafx.fxml.FXML
@@ -35,9 +39,53 @@ public class CO_Goal3_ViewController
     private Label messageContentLabel;
     @javafx.fxml.FXML
     private Label senderNameLabel;
+    @javafx.fxml.FXML
+    private TableColumn senderIdTableColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateTableColumn;
+    private ArrayList<Message> messageList = new ArrayList<>();
+
 
     @javafx.fxml.FXML
     public void initialize() {
+        missionTypeTableColumn.setCellValueFactory(new PropertyValueFactory<>("missionType"));
+        subjectTableColumn.setCellValueFactory(new PropertyValueFactory<>("subject"));
+        currentStatusTableColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+        messageIdTableColumn.setCellValueFactory(new PropertyValueFactory<>("messageId"));
+        missionIdTableColumn.setCellValueFactory(new PropertyValueFactory<>("missionId"));
+        senderIdTableColumn.setCellValueFactory(new PropertyValueFactory<>("senderId"));
+        dateTableColumn.setCellValueFactory(new PropertyValueFactory<>("timestamp"));
+
+        File file = new File("Message.bin");
+        if (!file.exists()) {
+            System.out.println("File not found, returning empty list.");
+            return;
+        }
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            while (true){
+                try {
+                    Message m1 = (Message) ois.readObject();
+                    messageList.add(m1);
+                } catch (EOFException e) {
+                    System.out.println("Bin file read!");
+                    break;
+                }
+            }
+        }
+        catch (Exception e) {
+            System.out.println("error");
+        }
+        ArrayList<Message> tempList = new ArrayList<>();
+        for(Message m : messageList){
+            if (m.getStatus().equals("UNREAD")){
+                tempList.add(m);
+            }
+
+        }
+        incomingMessagesTableView.getItems().addAll(tempList);
+
     }
 
     @javafx.fxml.FXML
