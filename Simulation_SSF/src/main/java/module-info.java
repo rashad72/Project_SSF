5b5dbc1d6bc73589e4f33654bfe262a740300763
toufikur-r38 <toufikur.private@gmail.com>
2025-12-08 23:24:49 +0600

    module error solved

--- Simulation_SSF/src/main/java/module-info.java
+++ Simulation_SSF/src/main/java/module-info.java
@@ -5,11 +5,15 @@ module com.example.simulation_ssf {
 
 
     opens com.example.simulation_ssf to javafx.fxml;
+    // new add
     opens com.example.simulation_ssf.Controller to javafx.fxml;
+    opens com.example.simulation_ssf.nonUser to javafx.base;
+    //new
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser1 to javafx.fxml;
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser2 to javafx.fxml;
     opens com.example.simulation_ssf.Controller.FXMLControllerForUser3 to javafx.fxml;
     opens com.example.simulation_ssf.Controller.FXMLControllerForUser4 to javafx.fxml;
-    opens com.example.simulation_ssf.User to javafx.fxml;
-    opens com.example.simulation_ssf.nonUser to javafx.fxml;
-
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser5 to javafx.fxml;
+    opens com.example.simulation_ssf.Controller.FXMLControllerForUser6 to javafx.fxml;
     exports com.example.simulation_ssf;
 }
